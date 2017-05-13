462080605da9c2520f29ec86692aa0792cd3d615
AlfredYan <1187108217@qq.com>
2017-05-13 14:28:40 -0500

    add insertion and selection

--- src/SearchAndSortAlgorithm.java
+++ src/SearchAndSortAlgorithm.java
@@ -93,6 +93,7 @@ public class SearchAndSortAlgorithm {
 		theArray[indexTwo] = tempValue;
 	}
 	
+	//Linear works better when needs to match all
 	public String linearSearch(int value){
 		
 		boolean valueInArray = false;
@@ -119,6 +120,8 @@ public class SearchAndSortAlgorithm {
 		return indexOfValue;
 	}
 	
+	//Binary needs values are sorted and quicker than Linear
+	//start searching from the middle
 	public void binarySearch(int value){
 		
 		int lowIndex = 0;
@@ -157,13 +160,13 @@ public class SearchAndSortAlgorithm {
 	public void bubbleSort(){
 		
 		/*
-		 * i starts at the end of the array
+		 * "i" starts at the end of the array
 		 * it is used to decrease the indexes which have been sorted
 		 */
 		for(int i=arraySize-1; i>0; i--){
 			
 			/*
-			 * j starts at the beginning of the array
+			 * "j" starts at the beginning of the array
 			 * the loop compares each value next to each other
 			 * if value is greater, then swap them
 			 */
@@ -178,6 +181,71 @@ public class SearchAndSortAlgorithm {
 		}
 	}
 	
+	/*
+	 * find the minimum value for every index by searching the unsorted part of array
+	 * and put the whole entire array in order
+	 * ASC the array by selection sort 
+	 */
+	public void selectionSort(){
+		
+		//sort at the beginning of the array
+		for(int i=0; i<arraySize; i++){
+			
+			int minimum = i;
+			
+			/*
+			 * if the value of "minimum" greater than the value of "j"
+			 * assign "j" to "minimum"
+			 * when the loop finishes, swap the value of "i" and "minimum"
+			 */
+			for(int j=i; j<arraySize; j++){
+				
+				//To have a DESC sort, change ">" to "<"
+				if(theArray[minimum] > theArray[j]){
+					minimum = j;
+				}
+			}
+			
+			swapValues(i, minimum);
+			printHorzArray(i, -1);
+		}
+	}
+	
+	/*
+	 * normally the best of all elementary sorts
+	 * ASC the array by insertion sort
+	 */
+	public void insertionSort(){
+		
+		for(int i=1; i<arraySize; i++){
+			int j = i;
+			
+			//"toInsert" is used to assign to the appropriate index after WHILE loop
+			int toInsert = theArray[i];
+			
+			/*
+			 * if the value of "j-1" greater than "toInsert",
+			 * assign "j-1" to "j"
+			 */
+			//To have a DESC sort, change second ">" to "<"
+			while((j>0)&&(theArray[j-1] > toInsert)){
+				
+				theArray[j] = theArray[j-1];
+				j--;
+				
+				printHorzArray(i, j);
+			}
+			
+			theArray[j] = toInsert;
+			printHorzArray(i, j);
+			
+			System.out.println("Array[i] = "+theArray[i]
+					+", Array[j] = "+theArray[j]
+					+", toInsert = "+toInsert);
+			
+		}
+	}
+	
 	
 	
 	
@@ -187,20 +255,34 @@ public class SearchAndSortAlgorithm {
 		
 		newArray.generateRandomArray();
 		
+		System.out.println("Here is the array with 10 ramdon numbers: ");
 		newArray.printHorzArray(-1, -1);
 		
+		//Linear Search
 //		System.out.println("Linear Search Start...");
 //		newArray.linearSearch(15);
 //		System.out.println("Linear Search End...");
 		
-		System.out.println("Bubble Sort Start...");
-		newArray.bubbleSort();
-		System.out.println("Bubble Sort End...");
-		System.out.println();
+		//Bubble Sort
+//		System.out.println("Bubble Sort Start...");
+//		newArray.bubbleSort();
+//		System.out.println("Bubble Sort End...");
 		
-		System.out.println("Binary Search Start...");
-		newArray.binarySearch(15);;
-		System.out.println("Binary Search End...");
+		//Selection Sort
+//		System.out.println("Selection Sort Start...");
+//		newArray.selectionSort();
+//		System.out.println("Selection Sort End...");
+		
+		//Insertion Sort
+		System.out.println("Insertion Sort Start...");
+		newArray.insertionSort();
+		System.out.println("Insertion Sort End...");
+		
+		//Binary Search(uncomment one of the sort code before execute Binary)
+//		System.out.println();
+//		System.out.println("Binary Search Start...");
+//		newArray.binarySearch(15);
+//		System.out.println("Binary Search End...");
 	}
 }
 
